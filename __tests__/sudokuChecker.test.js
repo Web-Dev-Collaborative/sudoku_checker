d0c8a3f1f3c2efead63c864dafb888ffc7b5d05a
Usarneme <jamestcoop@gmail.com>
2021-04-14 09:37:27 -0700

    validation of puzzles now working

--- __tests__/sudokuChecker.test.js
+++ __tests__/sudokuChecker.test.js
@@ -7,32 +7,33 @@ describe("TEMPLATE TEST", () => {
   })
 })
 
-describe("sudokuChecker()", () => {
-  const validSudoku = [
-    // columns
-  // 0 1 2 3 4 5 6 7 8
-    [1,2,3,4,5,6,7,8,9], // row 0
-    [4,5,6,7,8,9,1,2,3], // row 1
-    [7,8,9,1,2,3,4,5,6],
-    [2,3,4,5,6,7,8,9,1],
-    [5,6,7,8,9,1,2,3,4],
-    [8,9,1,2,3,4,5,6,7],
-    [3,4,5,6,7,8,9,1,2],
-    [6,7,8,9,1,2,3,4,5],
-    [9,1,2,3,4,5,6,7,8],
-  ]
+const validSudoku = [
+  // columns
+// 0 1 2 3 4 5 6 7 8
+  [1,2,3,4,5,6,7,8,9], // row 0
+  [4,5,6,7,8,9,1,2,3], // row 1
+  [7,8,9,1,2,3,4,5,6],
+  [2,3,4,5,6,7,8,9,1],
+  [5,6,7,8,9,1,2,3,4],
+  [8,9,1,2,3,4,5,6,7],
+  [3,4,5,6,7,8,9,1,2],
+  [6,7,8,9,1,2,3,4,5],
+  [9,1,2,3,4,5,6,7,8], // row 8
+]
 
-  const invalidSudoku = [
-    [2,2,2,4,5,6,7,8,9],
-    [3,5,4,6,2,5,0,2,3],
-    [2,2,2,4,5,6,7,8,9],
-    [3,5,4,6,2,5,0,2,3],
-    [2,2,2,4,5,6,7,8,9],
-    [3,5,4,6,2,5,0,2,3],
-    [2,2,2,4,5,6,7,8,9],
-    [3,5,4,6,2,5,0,2,3],
-    [3,5,4,6,2,5,0,2,3],
-  ]
+const invalidSudoku = [
+  [1,2,3,4,5,6,7,8,9], // row 0
+  [4,5,6,7,8,9,1,2,3], // row 1
+  [7,8,9,1,2,3,4,5,6],
+  [2,3,4,5,6,7,8,9,1],
+  [5,6,7,8,9,1,2,3,4],
+  [8,9,1,2,3,4,5,6,7],
+  [3,4,5,6,7,8,9,1,2],
+  [6,7,8,9,1,2,3,4,5],
+  [9,1,2,3,4,5,6,7,9], // row 8- BAD VALUE HERE
+]
+
+describe("sudokuChecker()", () => {
 
   test("SudokuChecker exists", () => {
     const mySudokuChecker = new SudokuChecker()
@@ -45,17 +46,25 @@ describe("sudokuChecker()", () => {
   })
 })
 
-describe("SudokuChecker.Validator()", () => {
-  const validRow = [[1,2,3,4,5,6,7,8,9]]
-
-  test("SudokuChecker.Validator exists", () => {
+describe("SudokuChecker.validate()", () => {
+  test("SudokuChecker.validate exists", () => {
     const mySudokuChecker = new SudokuChecker()
-    expect(mySudokuChecker.validator).toBeDefined();
+    expect(mySudokuChecker.validate).toBeDefined();
   })
 
-  test("SudokuChecker.validator validates first row", () => {
-    const mySudokuChecker = new SudokuChecker(validRow)
-    expect(mySudokuChecker.validator()).toEqual(true)
+  test("SudokuChecker.validate validates first row", () => {
+    const mySudokuChecker = new SudokuChecker(validSudoku)
+    expect(mySudokuChecker.validate()).toEqual(true)
+  })
+
+  test("validate returns false on an invalid puzzle", () => {
+    const mySudokuChecker = new SudokuChecker(invalidSudoku)
+    expect(mySudokuChecker.validate()).toEqual(false)
+  })
+
+  test("validate returns false on an invalid puzzle with a deeply nested invalid value", () => {
+    const mySudokuChecker = new SudokuChecker(invalidSudoku)
+    expect(mySudokuChecker.validate()).toEqual(false)
   })
 })
 
--- src/sudokuChecker.js
+++ src/sudokuChecker.js
@@ -1,38 +1,60 @@
 export function SudokuChecker(puzzle) {
   this.puzzle = puzzle;
+  this.columns = []
+  this.squares = []
 }
 
-SudokuChecker.prototype.validator = function() {
-  // loop through each row
-  for (let i = 0; this.puzzle; i++) {
-    // make a copy of the current row
-    const row = [].concat(this.puzzle[i])
-    // while there are 2+ elements in the current row...
-    while (row.length > 1) {
-      const currentValue = row.shift() // kickoff the first element
-      // check if there are values in the row that are duplicate of the current value
-      if (row.includes(currentValue)) {
-        // then we have a duplicate value, IE NOT VALID
-        return false
-      }
+SudokuChecker.prototype.createColumns = function() {
+  for (let i = 0; i < this.puzzle.length; i++) {
+    for (let j = 0; j < this.puzzle.length; j++) {
+      this.columns.push(this.puzzle[j][i])
+    }
+  }
+}
+
+SudokuChecker.prototype.createSquares = function() {
+  // [0][0],[0][1],[0][2]
+  // [1][0],[1][1],[1][2]
+  // [2][0],[2][1],[2][2]
+  for (let i = 0; i < this.puzzle.length; i++) {
+    for (let j = 0; j < this.puzzle.length; j++) {
+      const gridIndex = (this.puzzle[i] / 3) * 3 + (this.puzzle[j] / 3)
+      if (this.squares[gridIndex] === undefined) this.squares[gridIndex] = []
+      this.squares[gridIndex].push(this.puzzle[i][j])
+    }
+  }
+}
+
+SudokuChecker.prototype.validate = function() {
+  // validate each row
+  for (let i = 0; i < this.puzzle.length; i++) {
+    if (!validateSection([].concat(this.puzzle[i]))) return false
+  }
+  // rows are now validated, create columns
+  this.createColumns()
+  // validate each column
+  for (let i = 0; i < this.columns.length; i++) {
+    if (!validateSection([].concat(this.columns[i]))) return false
+  }
+  // columns are now validates, create squares
+  this.createSquares()
+  // validate each square 3x3
+  for (let i = 0; i < this.squares.length; i++) {
+    if (!validateSection([].concat(this.squares[i]))) return false
+  }
+  return true
+}
+
+// Takes in an array with 9 values and looks for duplicates
+function validateSection(section) {
+  // while there are 2+ elements in the current section...
+  while (section.length > 1) {
+    const currentValue = section.shift() // kickoff the first element
+    // check if there are values in the section that are duplicate of the current value
+    if (section.includes(currentValue)) {
+      // then we have a duplicate value, IE NOT VALID
+      return false
     }
   }
   return true
-  // validate rows
-  // validate cols
-  // validate boxes
 }
-
-// const mySudokuChecker = new SudokuChecker([[1,2,3,4,5,6,7,8,9]])
-// mySudokuChecker.puzzle = [ [1,2,3,4,5,6,7,8,9], 1, 2, 3 ]
-// mySudokuChecker.puzzle[0] = [1,2,3,4,5,6,7,8,9]
-// mySudokuChecker.puzzle[1] = 1
-// mySudokuChecker.puzzle[2] = 2
-// mySudokuChecker.puzzle[4] = 3
-
-// go thru each row, and then check if that is valid
-// const array = [1,2,3]
-// const arrayCopy = [...array]
-// arrayCopy => [1,2,3]
-
-
